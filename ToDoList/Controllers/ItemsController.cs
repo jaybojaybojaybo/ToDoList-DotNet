a599cd851f4377ab2cb408e96e15a81085af3e30
Guest User <Guest@Epicodus-2E.local>
2018-04-16 11:21:34 -0700

    setup MVC for Items

---
+++ ToDoList/Controllers/ItemsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ToDoList.Controllers
+{
+    public class ItemsController : Controller
+    {
+        private ToDoListContext db = new ToDoListContext();
+
+        public IActionResult Index()
+        {
+            return View(db.Items.ToList());
+        }
+        public IActionResult Details(int id)
+        {
+            Item thisItem = db.Items.FirstOrDefault(items => items.ItemId == id);
+            return View(thisItem);
+        }
+    }
+}
---
+++ ToDoList/Models/ToDoListContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDoList.Models
+{
+    public class ToDoListContext : DbContext
+    {
+        public DbSet<Item> Items { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder
+            .UseMySql(@"Server=localhost;Port=8889;database=todolist;uid=root;pwd=root;");
+    }
+}
